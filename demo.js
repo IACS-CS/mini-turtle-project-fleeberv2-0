b43a560bbebeb74768e8e01adbb746945c2cee40
rmamet <179627394+rmamet@users.noreply.github.com>
2024-12-03 22:31:16 -0500

    add zigZagCount param

--- demo.js
+++ demo.js
@@ -38,7 +38,7 @@ export const runDemo = async (turtle, ti) => {
     await turtle.setLineWidth(3);
   };
 
-  const drawBag = async (size) => {
+  const drawBag = async (size, zigZagCount) => {
     await turtle.penUp();
     await turtle.setLineWidth(3);
     await turtle.right(90);
@@ -54,8 +54,8 @@ export const runDemo = async (turtle, ti) => {
     await turtle.right(90);
     await turtle.forward(120 * size);
     await turtle.right(135);
-    for (let n = 0; n < 6; n++) {
-      await makeZigZag(size);
+    for (let n = 0; n < zigZagCount / 2; n++) {
+      await makeZigZag(size, zigZagCount);
     }
     await turtle.closePath();
     await turtle.fill();
@@ -67,13 +67,13 @@ export const runDemo = async (turtle, ti) => {
     await turtle.right(90);
   };
 
-  const makeZigZag = async (size) => {
+  const makeZigZag = async (size, zigZagCount) => {
     // Distance found using pythagoras:
     // We know distance from top-left to top-right it 80
     // 80/12 to find one triangle
     // It's all 45 degrees, so Math.sqrt((20/3)**2 + (20/3)**2)
     // Simplify it out:
-    let zigDistance = (20 * Math.sqrt(2)) / 3;
+    let zigDistance = (80 * Math.sqrt(2)) / zigZagCount;
     await turtle.forward(zigDistance * size);
     await turtle.left(90);
     await turtle.forward(zigDistance * size);
@@ -90,6 +90,14 @@ export const runDemo = async (turtle, ti) => {
     let color = await ti.prompt(
       "What color should the bag be? (type black to end)"
     );
+    let zigZagCount = await ti.promptNumber(
+      "How many cuts should the bag have on the top? (must be an even number!)"
+    );
+    while (zigZagCount % 2 != 0) {
+      zigZagCount = await ti.promptNumber(
+        "How many cuts should the bag have on the top? (must be an even number!)"
+      );
+    }
     let shouldIContinue =
       size != 0 &&
       !["black", "#000000", "000000", "000", "#000"].includes(
@@ -97,7 +105,7 @@ export const runDemo = async (turtle, ti) => {
       );
     if (shouldIContinue) {
       await turtle.setFillStyle(color);
-      await drawBag(size);
+      await drawBag(size, zigZagCount);
       await drawColonFollowedByThree(size);
     } else {
       keepDrawing = false;
